2781082169ab0b1e1f2c1035ecdf05dcb70b1eb7
Abhishek Yadav <er.abhishek.yadav.eee@gmail.com>
2025-11-06 18:33:41 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,19 @@
+
+#Task_1
+first_num = float(input('enter first number: '))
+second_num = float(input('enter second number: '))
+Addition = first_num + second_num
+print('Addition: ', Addition)
+substraction = first_num - second_num
+print('Substraction: ', substraction)
+multiplication = first_num * second_num
+print('Multiplication: ', multiplication)
+division = first_num / second_num
+print('Division: ', round(division,1))
+
+#Task_2
+user_first_name = input('Enter your first name: ')
+user_second_name = input('Enter your second name: ')
+
+full_name = user_first_name + ' ' + user_second_name
+print("Hello, " + full_name + "! " + "Welcome to the python program.")
